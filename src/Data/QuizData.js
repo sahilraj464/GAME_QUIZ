41f2eb7e433ad635e88c29ac4c4279b0d7408bea
sinha2002mishika <sinha2002mishika@gmail.com>
2024-10-09 00:17:26 +0530

    Initial commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "Python is a _",
+        options: ["Low Level Langauge", "High Level Langauge", "Both", "none of the above"],
+        answer:2
+    },
+    {
+        question: "What does SQL stands for?",
+        options: ["Structured Query Language", "Strong Query Langauge", "Structured Question Language", "none of the above"],
+        answer:1
+    },
+];
